fcbeba838af4e8e7aa24a7b55c0c68e6724fb04a
Michael Uzoigwe <michaeluzoigwe78@gmail.com>
2024-10-10 11:38:56 +0100

    create test api route

--- api/app.js
+++ api/app.js
@@ -2,6 +2,7 @@ import express from 'express';
 import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+import userRoutes from './routes/userRoute.js'
 
 
 mongoose.connect(process.env.MONGO_URL)
@@ -19,4 +20,6 @@ const port = process.env.PORT
 app.listen(port, () => {
     console.log(`Server is listening on ${port}`);
 });
+
+app.use('/api/user', userRoutes);
 
