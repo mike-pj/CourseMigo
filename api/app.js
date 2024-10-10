7c5c8db6d9c9916f892e92da199717f09ce437ff
Michael Uzoigwe <michaeluzoigwe78@gmail.com>
2024-10-10 08:33:35 +0100

    set environment and connect to db

--- api/app.js
+++ api/app.js
@@ -1,6 +1,16 @@
 import express from 'express';
+import mongoose from 'mongoose';
 import dotenv from 'dotenv';
 dotenv.config();
+
+
+mongoose.connect(process.env.MONGO_URL)
+.then(() => {
+    console.log("Connected to the MongoDB Database")
+})
+.catch((err) => {
+    console.log(err.message)
+})
 
 const app = express();
 
